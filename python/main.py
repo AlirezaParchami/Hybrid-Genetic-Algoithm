ff4bdf9d9bb857fd1d5c169443cc5a6a00c749c8
alireza parchami <alirezaprm76@gmail.com>
2019-01-23 23:42:38 +0330

    some bugs fixed

--- python/main.py
+++ python/main.py
@@ -4,7 +4,7 @@ import math
 from random import randint
 
 filename = ""
-
+data = []
 
 def individual_generation(indiv_size):
     """create stochastic individuals"""
@@ -27,7 +27,7 @@ def reproduction(cross_prob, mut_prob, persons, person_fitness, parentPercent, o
     # Select parents, crossover, mutation
     children = []
     SP = parent_selection(persons, person_fitness, parentPercent, offspringPercent)  # Selected Parents
-    print("selected parents: ", SP)
+    #print("selected parents: ", SP)
     for x in SP:
         x[0], x[1] = crossover(cross_prob, indivSize, x[0], x[1])
         x[0] = mutation(mut_prob, indivSize, x[0])
@@ -39,48 +39,47 @@ def reproduction(cross_prob, mut_prob, persons, person_fitness, parentPercent, o
 
 def crossover(cross_prob, length, first, second):
     if random.uniform(0, 1) < cross_prob:
-        print("--------Crossover of: ", first, "  ", second)
+    #    print("--------Crossover of: ", first, "  ", second)
         crossover_points = random.sample(range(1, length), 2)
         crossover_points.sort()
         tmp = second
         second = second[:crossover_points[0]] + first[crossover_points[0]:crossover_points[1]] + second[crossover_points[1]:]
         first = first[:crossover_points[0]] + tmp[crossover_points[0]:crossover_points[1]] + first[crossover_points[1]:]
-        print("%%%Crossover Done: ", first, "  ", second)
+    #    print("%%%Crossover Done: ", first, "  ", second)
     return first, second
 
 
 def mutation(mut_prob, length, string):
     for i in range(0, len(string)):
         if random.uniform(0, 1) < mut_prob:
-            print("------------ Mutation of ", string)
-            switch_char(i, string)
-            print("%%%Mutation Done: ", string, "\n")
+    #        print("------------ Mutation of ", string)
+            string = switch_char(i, string)
+    #        print("%%%Mutation Done: ", string, "\n")
     return string
 
 
-def knapsnack(x):  # fn: file name
-    # Open file
-    data = open(filename, "r")
+def knapsnack(x):
+    global finfuncstart
+    finfuncstart += 1
     # Read first line and detect the number and MaxWeight
-    first_line = data.readline().split(" ")
-    numbers = int(first_line[0])
-    max_weight = int(first_line[1])
+    numbers = data[0][0]
+    max_weight = data[0][1]
     values = []
     weights = []
     # Read each line and calculate the weight and value for x
-    for line in data:
-        splited_data = line.split(" ")
-        values.append(int(splited_data[0]))
-        weights.append(int(splited_data[1]))
+    for line in range(1, len(data)):
+        values.append(data[line][0])
+        weights.append(data[line][1])
     sum_weight = 0
     sum_value = 0
     for i in range(0, len(x)):
         if x[i] == '1':
             sum_weight += weights[i]
             sum_value += values[i]
+    #print("SUM Weight: ", sum_weight)
+    #print("SUM Value: ", sum_value)
     if sum_weight > max_weight:
         sum_value = 0
-    data.close()
     return sum_value
 
 
@@ -88,9 +87,10 @@ def parent_selection(persons, person_fitness, parentPercent, offspringPercent):
     # List of Fitness percents
     fitness_percent = []
     fitness_sum = 0
-    print("Person Fitness in parent_selection func: ", person_fitness)
+    #print("Person Fitness in parent_selection func: ", person_fitness)
     for x in person_fitness:
         fitness_sum += x
+    #print("Fitness SUM: ", fitness_sum)
     tmp = 0
     for x in person_fitness:
         tmp += (x/fitness_sum)
@@ -136,22 +136,31 @@ def neighbor_values(mychild):
 
 
 def hill_climbing(mychild, improve, sideway, tabu):
+
+    #print("------------Local search: ", mychild)
     if improve > MaxImprove:
         return mychild
     child_value = knapsnack(mychild)
     sorted_values = neighbor_values(mychild)
     improved_child = ""
     if sorted_values[0][1] > child_value:
+    #    print("improve")
         improve += 1
         improved_child = hill_climbing(sorted_values[0][0], improve, sideway, tabu)
     elif sorted_values[0][1] < child_value:
+    #    print("return")
         improved_child = mychild
     else:
-        for x in sorted_values:
-            if x[0] not in tabu and x[1] == child_value:
-                tabu.append(child)
-                sideway += 1
-                improved_child = hill_climbing(x[0], improve, sideway, tabu)
+    #    print("sideway")
+        if sideway > MaxSideWay:
+            improved_child = mychild
+        else:
+            for x in sorted_values:
+                if x[0] not in tabu and x[1] == child_value:
+                    tabu.append(child)
+                    sideway += 1
+                    improved_child = hill_climbing(x[0], improve, sideway, tabu)
+    #print("%%% Hill climbing Done: ", improved_child)
     return improved_child
 
 
@@ -165,7 +174,23 @@ def replacement(persons_list):
     selected = []
     for i in range(0, popSize):
         selected.append(tmp[i][0])
-    return selected
+    ave = 0
+    for i in range(0, popSize):
+        ave += tmp[i][1]
+    ave = ave / popSize
+    return selected, ave
+
+def read_file():
+    d = open(filename, "r")
+    first_line = d.readline().split(" ")
+    tmp = (int(first_line[0]), int(first_line[1]))
+    data.append(tmp)
+    for line in d:
+        splited_data = line.split(" ")
+        tmp = (int(splited_data[0]), int(splited_data[1]))
+        data.append(tmp)
+
+
 
 
 """
@@ -185,35 +210,49 @@ print(a[1:])
 # 1 2 3 4 5 6 7 8 9
 
 """
-indivSize = int(input("Enter the length of Individual: "))
-popSize = int(input("Enter the number of population: "))
-persons = population_generation(popSize, indivSize)
-print("persons= ", persons)
+mode = int(input("Enter Mode: 1-Genetic 2-A* : "))
+if mode == 1:
+    finfuncstart = 0
+    indivSize = int(input("Enter the length of Individual: "))
+    popSize = int(input("Enter the number of population: "))
+    persons = population_generation(popSize, indivSize)
+    print("persons= ", persons)
 
-filename = input("Enter the name of your file: ")
-filename = "..//Dataset//" + filename + ".txt"
-parentPercent = float(input("Enter parent percent: "))
-offspringPercent = float(input("Enter offspring percent: "))
-maxGen = int(input("Enter max generation number: "))
-crossoverProb = float(input("Enter Crossover Probability: "))
-mutaionProb = float(input("Enter Mutation Probability: "))
-TabuSize = int(input("Enter Tabu Size: "))
-MaxSideWay = int(input("Enter Max Sizeway: "))
-MaxImprove = int(input("Enter Max Improve: "))
+    filename = input("Enter the name of your file: ")
+    filename = "..//Dataset//" + filename + ".txt"
+    parentPercent = float(input("Enter parent percent: "))
+    offspringPercent = float(input("Enter offspring percent: "))
+    maxGen = int(input("Enter max generation number: "))
+    crossoverProb = float(input("Enter Crossover Probability: "))
+    mutaionProb = float(input("Enter Mutation Probability: "))
+    TabuSize = int(input("Enter Tabu Size: "))
+    MaxSideWay = int(input("Enter Max Sizeway: "))
+    MaxImprove = int(input("Enter Max Improve: "))
+    read_file()
+    #print("DATA: ", data)
+    while maxGen > 0:
+        #print("=========================================== maxGen: ", maxGen)
+        person_fitness = []
+        #print("Persons: ", persons)
+        for person in persons:
+            person_fitness.append(knapsnack(person))
+        #print("Person Fitness: ", person_fitness)
+        children = reproduction(crossoverProb, mutaionProb, persons, person_fitness, parentPercent, offspringPercent)
+        #print("Children: ", children)
+        improved_children = []
+        for child in children:
+            tabu = []
+            improved_children.append(hill_climbing(child, 0, 0, tabu))
+        persons = persons + improved_children
+        persons, average = replacement(persons)
+        maxGen -= 1
 
-while maxGen > 0:
-    print("=========================================== maxGen: ", maxGen)
-    person_fitness = []
-    print("Persons: ", persons)
-    for person in persons:
-        person_fitness.append(knapsnack(person))
-    print("Person Fitness: ", person_fitness)
-    children = reproduction(crossoverProb, mutaionProb, persons, person_fitness, parentPercent, offspringPercent)
-    print("Children: ", children)
-    improved_children = []
-    for child in children:
-        tabu = []
-        improved_children.append(hill_climbing(child, 0, 0, tabu))
-    persons = persons + improved_children
-    persons = replacement(persons)
-    maxGen -= 1
+    print("BEST Person: ", persons[0])
+    print("Best Person Fitness: ", knapsnack(persons[0]))
+    print("Fitness average: ", average)
+
+elif mode ==2 :
+    filename = input("Enter the name of your file: ")
+    filename = "..//Dataset//" + filename + ".txt"
+    read_file()
+    persons=[]
